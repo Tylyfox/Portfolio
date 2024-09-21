6e37a9ce92d09cf441d5c52ca693e130571ed58d
cadart <cadart.valentin@sfr.fr>
2024-09-21 23:33:35 +0200

    finish header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { AppWrap } from '../../wrapper';
 import { motion } from "framer-motion";
 import './Header.scss';
@@ -7,6 +6,7 @@ import { images } from "../../constants";
 import { SiGraphql } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
 import { SiPostgresql } from "react-icons/si";
+import { FaHandPeace } from "react-icons/fa"; // Nouvelle icône
 
 const scaleVariants = {
     whileInView: {
@@ -21,53 +21,68 @@ const scaleVariants = {
 
 const Header = () => {
     return (
-        <div className="app__header app__flex">
-            <div className="app__header-info">
-                <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    transition={{ duration: 1, ease: 'easeInOut' }}
-                    className="app__header-img"
-                >
-                    <div className="app__header-img-container">
-                        <img src={images.profile} alt="profile_bg" />
-                    </div>
-                    <div className="badge-cmp app__flex">
-                        <span>✌️</span>
-                        <div style={{marginLeft: 20}}>
-                            <p className="p-text">Salut, je suis</p>
-                            <h1 className="head-text">Valentin</h1>
-                        </div>
-                    </div>
-                </motion.div>
-                <motion.div
-                    variant={scaleVariants}
-                    whileInView={scaleVariants.whileInView}
-                    className="app__header-circles"
-                >
-                    {[<SiGraphql />, <FaNodeJs />, <SiPostgresql />].map((Icon, index) => (
-                      <div className="circle-cmp app__flex" key={`circle-${index}`}>
-                          {Icon}
+      <div className="app__header app__flex">
+          <div className="app__header-info">
+              <motion.div
+                initial={{ scale: 0 }}
+                animate={{ rotate: 360, scale: 1 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20
+                }}
+                className="app__header-img"
+              >
+                  <motion.div
+                    className="app__header-img-container"
+                    whileHover={{ scale: 1.0 }}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                      <img src={images.profile} alt="profile_bg" />
+                  </motion.div>
+                  <motion.div
+                    className="badge-cmp app__flex"
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                  >
+                      <FaHandPeace size={30} color="#95C082"/>
+                      <div style={{ marginLeft: 20 }}>
+                          <p className="p-text">Salut, je suis</p>
+                          <h1 className="head-text">Valentin</h1>
                       </div>
-                    ))}
-                </motion.div>
-            </div>
-            <motion.div
-                whileInView={{x: [-120, 0], opacity: [0, 1]}}
-                transition={{duration: 1}}
-                className="app__header-info"
-            >
-                <div className="app__header-badge">
-                    <div className="tag-cmp app__flex">
-                        <h1 className="head-text">Développeur Full-Stack</h1>
-                        <br/>
-                        <p className="p-text">Passionné par le développement web, je recherche une alternance en tant que concepteur développeur d'application, spécialisé en Node.js et React.</p>
-                        <p className="p-text">Diplômé en juin 2022 en tant que développeur Web Fullstack, j'ai récemment complété ma formation avec Angular et Spring Boot.</p>
-                        <p className="p-text"> Mon parcours m'a permis d'acquérir des compétences solides que je suis impatient de mettre en pratique.</p>
-                        <p className="p-text">Mon programme de formation à la Wild Code School est prévu du 13 novembre 2023 au 7 novembre 2024.</p>
-                    </div>
-                </div>
-            </motion.div>
-        </div>
+                  </motion.div>
+              </motion.div>
+              <motion.div
+                variant={scaleVariants}
+                whileInView={scaleVariants.whileInView}
+                className="app__header-circles"
+              >
+                  {[SiGraphql, FaNodeJs, SiPostgresql].map((Icon, index) => (
+                    <motion.div
+                      className="circle-cmp app__flex"
+                      key={`circle-${index}`}
+                      whileHover={{ scale: 1.2 }}
+                      whileTap={{ scale: 0.9 }}
+                    >
+                        <Icon size={60} />
+                    </motion.div>
+                  ))}
+              </motion.div>
+          </div>
+          <motion.div
+            whileInView={{ x: [-120, 0], opacity: [0, 1] }}
+            transition={{ duration: 1 }}
+            className="app__header-info"
+          >
+              <div className="app__header-badge">
+                  <div className="tag-cmp app__flex">
+                      <h1 className="head-text">Développeur Full Stack</h1>
+                      <p className="p-text">Passionné par les technologies et les défis techniques, je suis constamment à la recherche de solutions optimisées et innovantes. Mon alternance m'a permis de renforcer mes compétences, autant en back-end qu'en front-end. Polyvalent et adaptable, j'aime collaborer au sein d'équipes pour transformer des idées en solutions concrètes.</p>
+                  </div>
+              </div>
+          </motion.div>
+      </div>
     );
 }
 
