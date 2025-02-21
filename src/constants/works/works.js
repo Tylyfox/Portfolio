009006bd0d2f27653e754881c0f429ee47a6bf4c
Cadart <cadart.val@gmail.com>
2025-02-21 11:31:01 +0100

    remove works

--- src/constants/works/works.js
+++ src/constants/works/works.js
@@ -4,8 +4,6 @@ import greenride from '../../assets/videos/greenride.mp4';
 
 import KIT from '../../assets/works/KIT.webp';
 import GPT3 from '../../assets/works/gpt3.webp';
-import ADVICE from '../../assets/works/adviceGenerator.webp';
-import QR from '../../assets/works/QRCode.webp';
 import Rating from '../../assets/works/Rating.webp';
 import Portfolio from '../../assets/works/Portfolio.webp';
 import Propar from '../../assets/works/Propar.webp';
@@ -83,26 +81,6 @@ const works = [
         projectLink: 'https://tylyfox.github.io/GPT3-Angular-Front/',
         githubLink: 'https://github.com/Tylyfox/GPT3-Angular-Front',
         tags: ['Angular', 'Web App', 'All', 'UI/UX']
-    },
-    {
-        name: 'Advice Generator',
-        title: 'Advice Generator',
-        description: "Petit défi réalisé sur FrontEndMentor.io : générateur de conseils aléatoires",
-        imgUrl: ADVICE,
-        videoUrl: '',
-        projectLink: 'https://tylyfox.github.io/advice-generator-app-main/',
-        githubLink: 'https://github.com/Tylyfox/advice-generator-app-main',
-        tags: ['HTML/CSS/Javascript', 'All', 'UI/UX']
-    },
-    {
-        name: 'QR Code Component',
-        title: 'QR Code Component',
-        description: "Petit défi réalisé sur FrontEndMentor.io : composant QR Code",
-        imgUrl: QR,
-        videoUrl: '',
-        projectLink: 'https://tylyfox.github.io/-FrontEndMentorTylyfox_qr-code-component-main/',
-        githubLink: 'https://github.com/Tylyfox/-FrontEndMentorTylyfox_qr-code-component-main',
-        tags: ['HTML/CSS/Javascript', 'All', 'UI/UX']
     },
     {
         name: 'Interactive Rating Component',
